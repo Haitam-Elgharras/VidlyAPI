d23ddc4eef4f3b70a622010c67a61b8174e325a2
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-08-21 18:18:27 +0100

    integration test for routes with params

--- tests/intergration/genres.test.js
+++ tests/intergration/genres.test.js
@@ -1,6 +1,16 @@
 const request = require("supertest");
+const { Genre } = require("../../models/genres");
 
 let server;
+
+/*
+
+Notes:
+  
+  1- Tests should be isolated from each other.
+
+
+*/
 
 describe("/vidly/api/genres", () => {
   beforeEach(() => {
@@ -8,12 +18,50 @@ describe("/vidly/api/genres", () => {
   });
   afterEach(async () => {
     server.close();
+    //clean up the database
+    await Genre.deleteMany({});
   });
 
   describe("GET /", () => {
     it("should return all genres", async () => {
+      //populate the database with some genres
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+        { name: "genre3" },
+      ]);
+
       const res = await request(server).get("/vidly/api/genres");
+
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(3);
+      expect(res.body.some((g) => g.name === "genre3")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return a genre if it's exists", async () => {
+      const insertedGenre = new Genre({ name: "insertedGenre" });
+      await insertedGenre.save();
+
+      const res = await request(server).get(
+        `/vidly/api/genres/${insertedGenre._id}`
+      );
+      expect(res.status).toBe(200);
+      expect(res.body.name).toBe(insertedGenre.name);
+    });
+
+    it("should return 404  if the genre doesn't exist", async () => {
+      const notInsertedGenre = new Genre({ name: "notInserted" });
+      const res = await request(server).get(
+        `/vidly/api/genres/${notInsertedGenre._id}`
+      );
+      expect(res.status).toBe(404);
+    });
+
+    it("should return 404  if the id we passed is not a mongodb object id", async () => {
+      const res = await request(server).get(`/vidly/api/genres/1`);
+      expect(res.status).toBe(404);
     });
   });
 });
