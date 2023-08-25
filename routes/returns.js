dd910458db1538abd4590d9dfc8740fe1f171543
Haitam Elgharras <elgharras.haitam@gmail.com>
2023-08-25 21:06:22 +0100

    Test Driven refactoring

--- routes/returns.js
+++ routes/returns.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 
 router.post("/", (req, res) => {
+  if (!req.body.customerId || !req.body.movieId)
+    return res.status(400).send("Bad request");
+
   res.status(401).send("Unauthorized");
 });
 
--- tests/intergration/returns.test.js
+++ tests/intergration/returns.test.js
@@ -1,18 +1,28 @@
 const request = require("supertest");
 const { Rental } = require("../../models/rental");
 const mongoose = require("mongoose");
+const { User } = require("../../models/user");
 
 describe("/vidly/api/returns", () => {
   let server;
   let customerId;
   let movieId;
   let rental;
+  let token;
+
+  const exec = () => {
+    return request(server)
+      .post("/vidly/api/returns")
+      .set("x-auth-token", token)
+      .send({ customerId, movieId });
+  };
 
   beforeEach(async () => {
     server = require("../../index");
 
     customerId = new mongoose.Types.ObjectId();
     movieId = new mongoose.Types.ObjectId();
+    token = new User().generateAuthToken();
 
     rental = new Rental({
       customer: {
@@ -36,10 +46,26 @@ describe("/vidly/api/returns", () => {
   });
 
   it("should return 401 if client is not logged in", async () => {
-    const res = await request(server)
-      .post("/vidly/api/returns")
-      .send({ customerId, movieId });
+    token = "";
+
+    const res = await exec();
 
     expect(res.status).toBe(401);
+  });
+
+  it("return 400 if customerId is not provided", async () => {
+    customerId = "";
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+  });
+
+  it("return 400 if movieId is not provided", async () => {
+    movieId = "";
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
   });
 });
